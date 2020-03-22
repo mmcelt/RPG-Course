ebd8291ebb656ac96edfbb8fc19e1b6145b80528
Ron Welch <mmcelt@yahoo.com>
2020-03-22 14:20:39 -0600

    11 - Move using NavMshAgent

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] Transform _target;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		GetComponent<NavMeshAgent>().destination = _target.position;
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
