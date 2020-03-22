97c84de6769949da9f8af8274df21c2aaab03898
Ron Welch <mmcelt@yahoo.com>
2020-03-22 16:25:43 -0600

    15 - Create a Fixed Follow Camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] Transform _target;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		transform.position = _target.position;
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
