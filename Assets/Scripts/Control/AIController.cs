214c10e187b5e619cab43e4908e0729163cf5dca
Ron Welch <mmcelt@yahoo.com>
2020-03-29 16:17:30 -0600

    SECTION 5 - ENEMY AI: 46 - Calculating AI Chase Distance

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+	public class AIController : MonoBehaviour
+	{
+		#region Fields
+
+		[SerializeField] float _chaseDistance = 5f;
+
+		GameObject _player;
+
+		#endregion
+
+		#region MonoBehaviour Methods
+
+		void Start()
+		{
+			_player = GameObject.FindGameObjectWithTag("Player");
+		}
+
+		void Update()
+		{
+			if (DistanceToPlayer() < _chaseDistance)
+			{
+				print(transform.name + " Sees the Player, I'll get him!");
+			}
+		}
+
+		float DistanceToPlayer()
+		{
+			return Vector3.Distance(transform.position, _player.transform.position);
+		}
+		#endregion
+
+		#region Public Methods
+
+
+		#endregion
+
+		#region Private Methods
+
+
+		#endregion
+	}
+}
