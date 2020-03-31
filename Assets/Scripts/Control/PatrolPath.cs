d2047fba924636801b06dcdd94e01eea0b2b1429
Ron Welch <mmcelt@yahoo.com>
2020-03-31 16:57:06 -0600

    52 - Visualizing Waypoints

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+	public class PatrolPath : MonoBehaviour
+	{
+		#region Fields
+
+		const float _waypointGizmoSize = 0.25f;
+
+		#endregion
+
+		#region MonoBehaviour Methods
+
+		void Start()
+		{
+
+		}
+
+		void OnDrawGizmos()
+		{
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				Gizmos.DrawSphere(transform.GetChild(i).transform.position, _waypointGizmoSize);
+			}
+		}
+		#endregion
+
+		#region Public Methods
+
+
+		#endregion
+
+		#region Private Methods
+
+
+		#endregion
+	}
+}
