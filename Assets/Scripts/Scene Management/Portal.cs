993912ffb2e0c6d48438b237c1d858c5c36a98bc
Ron Welch <mmcelt@yahoo.com>
2020-04-03 10:46:16 -0600

    70 - Scene Loading Portals

---
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+	public class Portal : MonoBehaviour
+	{
+		#region Fields
+
+		[SerializeField] int _sceneToLoadIndex;
+
+		#endregion
+
+		#region MonoBehaviour Methods
+
+		void Start()
+		{
+
+		}
+
+		void OnTriggerEnter(Collider other)
+		{
+			if (other.CompareTag("Player"))
+			{
+				SceneManager.LoadScene(_sceneToLoadIndex);
+			}
+		}
+		#endregion
+
+		#region Public Methods
+
+
+		#endregion
+
+		#region Private Methods
+
+
+		#endregion
+	}
+}
