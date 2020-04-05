89e64080b2485d0ee0d13652ff88d01908b6050d
Ron Welch <mmcelt@yahoo.com>
2020-04-05 10:29:24 -0600

    81 - Savable Components & GameObjects

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using RPG.Core;
+using RPG.Saving;
 
 namespace RPG.Movement
 {
-	public class Mover : MonoBehaviour, IAction
+	public class Mover : MonoBehaviour, IAction, ISaveable
 	{
 		#region Fields
 
@@ -55,6 +56,19 @@ namespace RPG.Movement
 		public void Cancel()
 		{
 			_navAgent.isStopped = true;
+		}
+
+		public object CaptureState()
+		{
+			return new SerializableVector3(transform.position);
+		}
+
+		public void RestoreState(object state)
+		{
+			SerializableVector3 position = (SerializableVector3)state;
+			_navAgent.enabled = false;
+			transform.position = position.ToVector();
+			_navAgent.enabled = true;
 		}
 		#endregion
 
