4925f534a68373f0be9f27979ebd271d2b13ea9c
Ron Welch <mmcelt@yahoo.com>
2020-04-05 10:02:57 -0600

    80 - Triggering Saving & Loading

---
+++ Assets/Scripts/Scene Management/SavingWrapper.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.SceneManagement
+{
+	public class SavingWrapper : MonoBehaviour
+	{
+		#region Fields
+
+		const string _defaultSaveFile = "save";
+
+		#endregion
+
+		#region MonoBehaviour Methods
+
+		void Start()
+		{
+
+		}
+
+		void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.L))
+			{
+				Load();
+			}
+			if (Input.GetKeyDown(KeyCode.S))
+			{
+				Save();
+			}
+		}
+		#endregion
+
+		#region Public Methods
+
+
+		#endregion
+
+		#region Private Methods
+
+		void Load()
+		{
+			GetComponent<SavingSystem>().Load(_defaultSaveFile);
+		}
+
+		void Save()
+		{
+			GetComponent<SavingSystem>().Save(_defaultSaveFile);
+		}
+		#endregion
+	}
+}
